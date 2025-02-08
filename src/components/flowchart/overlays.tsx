d1802dcbc3cafea07a857a3b8597467a0f331e66
Bayode Emmanuel Olaseeni <obcbeats@gmail.com>
2025-02-09 01:24:53 +0300

    feat: complete overlays for chart
    - add overlays for all nodes

--- src/components/flowchart/overlays.tsx
+++ src/components/flowchart/overlays.tsx
@@ -14,7 +14,7 @@ export const OverlayZero = ({
         left: `${-position.x + 50}px`,
       }}
     >
-      <div className="min-w-1/2 mb-3 rounded-[6px] bg-[#FFF1F0]">
+      <div className="mb-3 w-1/2 rounded-[6px] bg-[#FFF1F0]">
         <p className="text-[13px] font-semibold leading-[22px] text-[#E5372B]">
           Lorem Ipsum Dolor Sit{" "}
         </p>
@@ -33,7 +33,7 @@ export const OverlayZero = ({
           ),
         )}
       </div>
-      <div className="rounded-6 w-fit rounded-full bg-[#F2EDFF] px-1 py-0.5">
+      <div className="rounded-6 w-fit rounded-[5px] bg-[#F2EDFF] px-1 py-0.5">
         <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
           Lorem: 1.2.3.4
         </p>
@@ -52,7 +52,168 @@ export const OverlayOne = ({
       className="w-fit rounded-[20px] bg-white px-[30px] pb-3 pt-5 shadow-lg"
       style={{
         top: `${position.y + 180}px`,
-        left: `${position.x}px`,
+        left: `${position.x - 100}px`,
+        position: "absolute",
+      }}
+    >
+      <div className="flex items-center gap-[14px]">
+        <div className="flex items-center justify-center rounded-full bg-brand-blue-faded p-3">
+          <IconServer />
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-secondary">Loremipsu </p>
+        </div>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2.5">
+        <div className="flex items-center gap-2">
+          <IconFile />
+          <p className="text-base font-bold text-[#525D73]">
+            Lorem: Loremipsum Loremipsum
+          </p>
+        </div>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2.5">
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export const OverlayTwo = ({
+  position,
+}: {
+  position: { x: number; y: number };
+}) => {
+  return (
+    <div
+      className="w-fit rounded-[20px] bg-white px-[30px] pb-3 pt-5 shadow-lg"
+      style={{
+        top: `${position.y + 180}px`,
+        left: `${position.x - 300}px`,
+        position: "absolute",
+      }}
+    >
+      <div className="flex items-center gap-[14px]">
+        <div className="flex items-center justify-center rounded-full bg-brand-blue-faded p-3">
+          <IconServer />
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-secondary">Loremipsum</p>
+        </div>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2">
+        <IconFile />
+        <p className="text-base font-bold text-[#525D73]">Lorem:</p>
+        <div className="rounded-[6px] bg-[#FFF9ED] px-2 py-0.5">
+          <p className="text-nowrap text-[13px] font-semibold leading-[22px] text-[#EBA622]">
+            Lorem &quot;ipsum&quot;
+          </p>
+        </div>
+        <div className="rounded-[6px] bg-[#E9FAF0] px-2 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#02983E]">
+            Lorem
+          </p>
+        </div>
+        <p className="text-base font-bold text-[#525D73]">
+          Loremipsum Loremipsum
+        </p>
+      </div>
+      <div className="mt-2.5 flex items-center gap-2.5">
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <div className="rounded-[6px] bg-[#ECF5FF] px-2 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#0053B5]">
+            Lorem 1234,5678
+          </p>
+        </div>
+      </div>
+      <div className="mt-2.5 flex items-center gap-2">
+        <IconFile />
+        <p className="text-base font-bold text-[#525D73]">Lorem:</p>
+        <div className="rounded-[6px] bg-[#FFF9ED] px-2 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#EBA622]">
+            Lorem &quot;ipsum&quot;
+          </p>
+        </div>
+        <p className="text-base font-bold text-[#525D73]">
+          Loremipsum Loremipsum
+        </p>
+      </div>
+      <div className="mt-2.5 flex items-center gap-2">
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>{" "}
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+        <div className="w-fit rounded-[6px] bg-[#F2EDFF] px-1 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#6236CC]">
+            1.2.3.4
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const OverlayThree = ({
+  position,
+}: {
+  position: { x: number; y: number };
+}) => {
+  return (
+    <div
+      className="w-fit rounded-[20px] bg-white px-[30px] pb-3 pt-5 shadow-lg"
+      style={{
+        top: `${position.y}px`,
+        left: `${position.x - 300}px`,
         position: "absolute",
       }}
     >
@@ -76,9 +237,9 @@ export const OverlayOne = ({
       </div>
 
       <div className="mt-2.5 flex items-center gap-2.5">
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <IconFile />
-          <p className="text-base font-bold text-[#525D73]">Lorem</p>
+          <p className="text-base font-bold text-[#525D73]">Lorem:</p>
         </div>
         <div className="min-w-[106px] rounded-[6px] bg-[#FFF9ED]">
           <p className="text-[13px] font-semibold leading-[22px] text-[#EBA622]">
@@ -88,40 +249,14 @@ export const OverlayOne = ({
       </div>
 
       <div className="mt-2.5 flex items-center gap-2.5">
-        <p className="text-base font-bold text-[#525D73]">Lorem</p>
-        <div className="min-w-[106px] rounded-[6px] bg-[#ECF5FF]">
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>
+        <div className="min-w-[106px] rounded-[6px] bg-[#ECF5FF] px-2 py-0.5">
           <p className="text-[13px] font-semibold leading-[22px] text-[#0053B5]">
-            lorem 1234,5678
+            Lorem 1234,5678
           </p>
         </div>
       </div>
     </div>
-  );
-};
-
-export const OverlayTwo = ({
-  position,
-}: {
-  position: { x: number; y: number };
-}) => {
-  return (
-    <div
-      className={`absolute bg-red-500`}
-      style={{ top: `${position.y + 180}px`, left: `${position.x}px` }}
-    ></div>
-  );
-};
-
-export const OverlayThree = ({
-  position,
-}: {
-  position: { x: number; y: number };
-}) => {
-  return (
-    <div
-      className={`absolute bg-red-500`}
-      style={{ top: `${position.y + 180}px`, left: `${position.x}px` }}
-    ></div>
   );
 };
 
@@ -132,8 +267,52 @@ export const OverlayFour = ({
 }) => {
   return (
     <div
-      className={`absolute bg-red-500`}
-      style={{ top: `${position.y + 180}px`, left: `${position.x}px` }}
-    ></div>
+      className="w-fit rounded-[20px] bg-white px-[30px] pb-3 pt-5 shadow-lg"
+      style={{
+        top: `${position.y + 100}px`,
+        left: `${position.x - 300}px`,
+        position: "absolute",
+      }}
+    >
+      <div className="flex items-center gap-[14px]">
+        <div className="relative flex items-center justify-center rounded-full bg-brand-blue-faded p-3">
+          <div
+            className={`absolute -right-2 -top-3 flex items-center justify-center rounded-full border border-white bg-brand-red p-1`}
+          >
+            <IconShieldX />
+          </div>
+          <IconServer />
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-secondary">
+            Loremipsumdolorsit002
+          </p>
+          <p className="text-xs font-semibold text-secondary-muted">
+            192.168.1.2
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2.5">
+        <div className="flex items-center gap-2">
+          <IconFile />
+          <p className="text-base font-bold text-[#525D73]">Lorem:</p>
+        </div>
+        <div className="min-w-[106px] rounded-[6px] bg-[#FFF9ED]">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#EBA622]">
+            Lorem &quot;ipsum&quot;
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-2.5 flex items-center gap-2.5">
+        <p className="text-base font-bold text-[#525D73]">Loremipsum</p>
+        <div className="min-w-[106px] rounded-[6px] bg-[#ECF5FF] px-2 py-0.5">
+          <p className="text-[13px] font-semibold leading-[22px] text-[#0053B5]">
+            Lorem 1234,5678
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
